20cf3321df4b8bfbfbb6c15d1555f8795b5bc0d4
Motoyuki Michiba <michiba.ipad@gmail.com>
2024-09-09 17:55:38 +0800

    vercel_test_00_00 : deploy html without sqlalchemy

---
+++ api/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("templates/login_signup.html", "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
+
+
+
+
+
+# from fastapi import FastAPI, Request
+# from fastapi.templating import Jinja2Templates
+# from fastapi.staticfiles import StaticFiles
+
+# app = FastAPI()
+
+# # Set up Jinja2 templates
+# templates = Jinja2Templates(directory="templates")
+
+# # Mount static files
+# # app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# @app.get("/")
+# async def login_signup(request: Request):
+#     message = "Please Log in or Sign up"
+#     message_color = "#0f0"
+#     return templates.TemplateResponse("test_00.html", {"request": request, "message": message, "message_color": message_color})
+
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# async def health_check():
+#     return "The health cjeck is successful!"
