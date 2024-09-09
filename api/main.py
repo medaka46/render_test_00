6beb276c4fe884b9817f7bb8b4d920a4f8a4b70f
Motoyuki Michiba <michiba.ipad@gmail.com>
2024-09-09 18:14:00 +0800

    vercel_test_00_01 : deploy html without sqlalchemy but with jinja2

--- api/main.py
+++ api/main.py
@@ -1,13 +1,49 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    with open("templates/login_signup.html", "r") as file:
-        html_content = file.read()
-    return HTMLResponse(content=html_content)
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def login_signup(request: Request):
+    message = "Please Log in or Sign up"
+    message_color = "#f00"
+    return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
+# 
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+# from fastapi.staticfiles import StaticFiles
+
+# app = FastAPI()
+
+# # Mount static files
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root():
+#     with open("static/test_00.html", "r") as file:
+#         html_content = file.read()
+#     return HTMLResponse(content=html_content)
+
+
+
+
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+
+# app = FastAPI()
+
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root():
+#     with open("templates/login_signup.html", "r") as file:
+#         html_content = file.read()
+#     return HTMLResponse(content=html_content)
 
 
 
