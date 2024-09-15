0f6d19e221dcc43e67d416f257469a6cb45dace3
Motoyuki Michiba <michiba.ipad@gmail.com>
2024-09-16 07:44:40 +0800

    vercel_test_00_05 : activate log in function

--- api/main.py
+++ api/main.py
@@ -110,230 +110,39 @@ async def add_user(request: Request, username: str = Form(...), email: str = For
     
 # --------------------
 
-# async def add_user(request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(None), db: Session = Depends(get_db)):
-#     db_user_check = db.query(User).filter(User.email == email).first()
-#     df_user = pd.read_csv('user.csv')
+start_date = datetime.today() - timedelta(days = datetime.today().weekday()) # set up monday is start_date
+
+date_sequence = [str((start_date + timedelta(days=i)).strftime('%Y-%m-%d')) for i in range(7*10)]  # 7*10 days sequence
+
+today_date = datetime.today().strftime('%Y-%m-%d')
+
+
+
+
+@app.post("/login_signup/check_user/")
+
+async def check_user(request: Request, date_sequence = date_sequence, today_date = today_date, username: str = Form(...), email: str = Form(...), password: str = Form(None), db: Session = Depends(get_db)):
+    # hashed_password = hash_password(password)
+    # login_username = username
+    # db_user = db.query(User).filter(User.username == username, User.email == email, User.password == hashed_password).first()
+    db_user = db.query(User).filter(User.username == username, User.email == email, User.password == password).first()
+    # db_user = db.query(User).filter(User.username == username and User.email == email and User.password == hashed_password).first()
+    # db_user = db.query(User).filter(User.username == username and User.email == email and User.password == hashed_password).first()
     
-#     logger.info(df_user)
+    print(User.password)
     
-#     if db_user_check:
-#         message = "Sign up failed. Mail address were already used."
-#         message_color = "#f00"
-#         return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-#     else:
-#         df_user_check = df_user[(df_user['username'] == username) & (df_user['email'] == email)]
-#         if not df_user_check.empty:
-#             message = "Sign up was accepted. Please proceed to Log in."
-#             message_color = "#0f0"
-            
-#             db_item = User(username=username, email=email, password=password)
-#             try:
-#                 db.add(db_item)
-#                 db.commit()
-#                 db.refresh(db_item)
-#             except Exception as e:
-#                 db.rollback()
-#                 message = f"An error occurred: {str(e)}"
-#                 message_color = "#f00"
-#                 logger.error(f"Database error: {str(e)}")
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-        
-#         else:
-#             message = "Sorry but you are not authorized to sign up."
-#             message_color = "#f00"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-
-
-# # --------------------
-
-
-# from fastapi import FastAPI, Depends, Request, Form
-# from fastapi.responses import HTMLResponse
-
-# from fastapi.templating import Jinja2Templates
-
-# from datetime import datetime, timedelta, timezone
-
-# from starlette.middleware.sessions import SessionMiddleware
-
-# from passlib.context import CryptContext
-
-# from .database import SessionLocal, engine, Base, Session
-
-# from .models import User, Link, Schedule, Project
-
-# import pandas as pd
-
-
-
-
-
-# # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-
-
-# app = FastAPI()
-
-# # Set up Jinja2 templates
-# templates = Jinja2Templates(directory="templates")
-
-# app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-# # def hash_password(password):
-# #     return pwd_context.hash(password)
-
-
-# @app.get("/")
-# async def login_signup(request: Request):
-#     message = "Please Log in or Sing up"
-#     message_color = "#0f0"
-    
-#     start_date_adjust = 0
-    
-#     # start_date = datetime.today() - timedelta(days = datetime.today().weekday()) # set up monday is start_date
-#     start_date = datetime.today() - timedelta(days = datetime.today().weekday() + start_date_adjust ) # set up monday is start_date
-#     date_sequence = [str((start_date + timedelta(days=i)).strftime('%Y-%m-%d')) for i in range(7*10)]  # 7*10 days sequence
-#     today_date = datetime.today().strftime('%Y-%m-%d')
-    
-#     request.session['start_date_adjust'] = start_date_adjust
-#     test_date = request.session.get('start_date_adjust')
-#     print(test_date)
-    
-#     request.session['link_tab_page_active'] = "link_001"
-    
-#     request.session['project_chart_switch'] = 1
-    
-#     request.session['project_button_serect'] = "ID"
-    
-#     request.session['project_id_order'] = 1
-    
-    
-    
-    
-#     # Your logic here, for example, returning a signup or login page
-#     # return templates.TemplateResponse("test01.html", {"request": request, "message": message, "message_color": message_color})
-#     # print("login_signup.html")
-#     return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-
-
-# @app.post("/login_signup/add_user/")
-
-# async def add_user(request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(None), db: Session = Depends(get_db)):
-#     # hashed_password = hash_password(password)
-    
-#     db_user_check = db.query(User).filter(User.email == email).first()
-#     df_user = pd.read_csv('user.csv')
-    
-#     print(df_user)
-    
-#     if db_user_check:
-#         message = "Sign up failed. Mail address were already used."
-#         message_color = "#f00"
-#         return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-#     else:
-#         df_user_check = df_user[(df_user['username'] == username) & (df_user['email'] == email)]
-#         if not df_user_check.empty:
-#             message = "Sign up was accepted. Please proceed to Log in."
-#             message_color = "#0f0"
-            
-#             db_item = User(username=username, email=email, password=password)
-#             try:
-#                 db.add(db_item)
-#                 db.commit()
-#                 db.refresh(db_item)
-#             except Exception as e:
-#                 db.rollback()
-#                 message = f"An error occurred: {str(e)}"
-#                 message_color = "#0f0"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-        
-#         else:
-#             message = "Sorry but you are not authorized to sign up."
-#             message_color = "#0f0"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
+    if db_user:
         
         
-# --------------------
-
-# async def add_user(request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(None), db: Session = Depends(get_db)):
-#     # hashed_password = hash_password(password)
-    
-#     db_user_check = db.query(User).filter(User.email == email).first()
-#     df_user = pd.read_csv('user.csv')
-    
-#     print(df_user)
-    
-#     if db_user_check:
-#         message = "Sign up failed. Mail address were already used."
-#         message_color = "#f00"
-#         return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-#     else:
-#         df_user_check = df_user[(df_user['username'] == username) & (df_user['email'] == email)]
-#         if not df_user_check.empty:
-#             message = "Sign up was accepted. Please proceed to Log in."
-#             message_color = "#0f0"
-            
-#             db_item = User(username=username, email=email, password=password)
-#             try:
-#                 db.add(db_item)
-#                 db.commit()
-#                 db.refresh(db_item)
-#             except Exception as e:
-#                 db.rollback()
-#                 message = f"An error occurred: {str(e)}"
-#                 message_color = "#0f0"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
+        login_username = username
+        time_zone_message = "Please select Time zone :"
+        message_color = "#f00"
         
-#         else:
-#             message = "Sorry but you are not authorized to sign up."
-#             message_color = "#f00"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
+        return templates.TemplateResponse("schedule_indicate_00.html", {"request": request, "dates": date_sequence, "today": today_date, "login_username": login_username, "time_zone_message": time_zone_message, "message_color": message_color})
+        # return templates.TemplateResponse("login_ok.html", {"request": request, "dates": date_sequence, "today": today_date})
+    else:
+        message = "Log in failed. Please try again"
+        message_color = "#f00"
+    # Your logic here, for example, returning a signup or login page
+        return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
 
-# async def add_user(request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(None), db: Session = Depends(get_db)):
-#     # hashed_password = hash_password(password)
-    
-#     db_user_check = db.query(User).filter(User.email == email).first()
-#     df_user = pd.read_csv('user.csv')
-    
-#     print(df_user)
-    
-#     if db_user_check:
-#         message = "Sign up failed. Mail address were already used."
-#         message_color = "#f00"
-#     # Your logic here, for example, returning a signup or login page
-#         return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-#     else:
-#         df_user_check = df_user[(df_user['username'] == username) & (df_user['email'] == email)]
-#         # if df_user_check:
-#         if not df_user_check.empty:
-#     # Your logic here
-#             message = "Sign up was accepted. Please procede Log in."
-#             message_color = "#0f0"
-            
-#             db_item = User(username=username, email=email, password=password)
-#             # db_item = User(username=username, email=email, password=hashed_password)
-#             db.add(db_item)
-#             db.commit()
-#             db.refresh(db_item)
-#         # Your logic here, for example, returning a signup or login page
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
-        
-#         else:
-#             message = "Sorry but you are not authorized to sign up."
-#             message_color = "#f00"
-            
-#             return templates.TemplateResponse("login_signup.html", {"request": request, "message": message, "message_color": message_color})
